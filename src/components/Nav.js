d355e855903e0bae60f9ab8adea713511ca88eab
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-11-15 16:03:44 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ const Nav = () => {
     return (
       <nav>
         <h2>EmojeeMarket</h2>
-        <a href="#" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://github.com/Abhishekjoshi98/react-emojeetask/" target="_blank" rel="noreferrer">GitHub</a>
       </nav>
     )
   }
