4ef575d35f3f3fecbbba9e86747d4bdc589d76ee
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-11-15 15:54:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+      <nav>
+        <h2>EmojeeMarket</h2>
+        <a href="#" target="_blank" rel="noreferrer">GitHub</a>
+      </nav>
+    )
+  }
+  
+  export default Nav;
